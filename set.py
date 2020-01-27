2f9e7b4c44ea265cabc976b3e364cd7210797c19
Prashant Shukla <shukla.prashant689@gmail.com>
2020-01-27 12:07:15 +0530

    Add files via upload

---
+++ set.py
@@ -0,0 +1,13 @@
+test = 1,2,3,2,4,'geeks','in','the','house.'		#one way of defining- separated by comas
+
+print(test)
+
+test1 = set('another')						#create set of charcter pf string								
+test2 = set('problem')
+print(test1)
+
+#--------------------operations
+
+print(test1 | test2)		# all the unique character set
+
+print(test1-test2)			# as planned
