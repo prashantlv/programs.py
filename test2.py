6a564a24432208ffde2fb133ff6c781e48b1aecd
Prashant Shukla <shukla.prashant689@gmail.com>
2020-03-15 20:24:32 +0530

    Add files via upload

---
+++ test2.py
@@ -0,0 +1,13 @@
+var = {
+    temp1 {
+        'title 0':2,
+        'title 1':3,
+        'title 2':4}
+        },
+    temp2 {
+        'title 3':12,
+        'title 4':13,
+        'title 5':14}
+    }
+
+print(var)  
