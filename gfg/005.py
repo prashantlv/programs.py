ad083fb04a9e830a15226fd142f55aac182b820c
prashantlv <shukla.prashant689@gmail.com>
2021-02-08 21:54:42 +0530

    $(curl -s whatthecommit.com/index.txt)

--- gfg/005.py
+++ gfg/005.py
@@ -1,14 +1,24 @@
 
-#code
+#code without algo---------------
 def main():
-    arr = list(map(int, input().split()))
-    a=[]
-    for i in range(len(arr)):
-        if arr[i] < 0:
-            a.insert(1, arr[i])
-        else:
-            a.insert(len(a), arr[i])
-    print(a)        
+	arr = list(map(int, input().split()))
+    #a=[]
+    #for i in range(len(arr)):
+    #    if arr[i] < 0:
+    #        a.insert(1, arr[i])
+    #    else:
+    #        a.insert(len(a), arr[i])
+    #print(a)
+#quick sort-------------------	
+	temp=None
+	j=0
+	for i in range(len(arr)):
+		if arr[i] < 0:
+			temp = arr[i]
+			arr[i]=arr[j]
+			arr[j]=temp
+			j += 1
+	print(arr)		        
 
 if __name__ == '__main__':
-    main()
+	main()
