e6acb4be50e99f094384cf26ca4033d1004f34d0
prashantlv <shukla.prashant689@gmail.com>
2021-02-21 00:32:40 +0530

    fixed

--- codeforces/617A.py
+++ codeforces/617A.py
@@ -1,20 +1,11 @@
 def main(a):
+	arr=[5,4,3,2,1]
 	count = 0
-	if a > 5:
-		count = count + a // 5
-		a = a%5
-	elif a > 4:
-		count = count + a // 4
-		a = a%4
-	elif a > 3:
-		count = count + a // 3
-		a = a%3
-	elif a == 2:
-		count += 1
-		a -= 2
-	else:
-		count += 1				
-	print(count)
+	for i in arr:
+		if a >= i:
+			count = count + a // i
+			a = a%i
+	print(count)		
 
 if __name__ == '__main__':
 	x = int(input())
