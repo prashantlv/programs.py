9721b5cf416c66cfd4d718b09b435ebc9e0b10cb
prashantlv <shukla.prashant689@gmail.com>
2021-02-27 14:14:43 +0530

    add another approach to 007

--- gfg/007.py
+++ gfg/007.py
@@ -1,3 +1,6 @@
+#approach 01 with pop()
+
+
 def rotate( arr, n):
     for i in range(1):
         arr.insert(0,arr.pop())
@@ -12,4 +15,16 @@ def main():
 
 if __name__ == '__main__':
 	main()	
+#----------------------------------------
+#approach 02 with iteration / another array
+def main(arr,k):
+	a,l = [], len(arr)
+	a = arr[-k:l]
+	a += arr[0:l-k]
+	print(a)
+
+if __name__ == '__main__':
+	arr = map(int, input().split())
+	k = int(input())
+	main(list(arr),k)
 
