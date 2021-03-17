5cfed5706ab7a7f8620165d687b842c80f6446ae
prashantshukla4@acm.org <prashantshukla4@acm.org>
2021-03-17 21:12:33 +0530

    bb

---
+++ gfg/010.py
@@ -0,0 +1,12 @@
+def main(l,arr):
+    numofjumps = []
+    minimumjumps = []
+    for i in range(l):
+        for j in range(i):
+            
+
+
+if __name__ =="__main__":
+    l = int(input())
+    arr = list(map(int, inut().split()))
+    main(l,arr)
