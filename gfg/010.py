37083d08f43dc78d7164d5e6b75234df3a6fce1b
maverick <shukla.prashant689@gmail.com>
2021-03-18 22:08:33 +0530

    aa

--- gfg/010.py
+++ gfg/010.py
@@ -1,12 +1,13 @@
 def main(l,arr):
-    numofjumps = []
-    minimumjumps = []
+    #numofjumps = []
+    minimumjumps = 0
     for i in range(l):
         for j in range(i):
-            
-
+           if arr[i]>=j:
+               minimumjumps += 1
+    print(minimumjumps)
 
 if __name__ =="__main__":
     l = int(input())
-    arr = list(map(int, inut().split()))
+    arr = list(map(int, input().split()))
     main(l,arr)
