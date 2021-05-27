69fd1c0b7b4257e2a17bf7a0a2db1cb8692a6fb7
maverick <shukla.prashant689@gmail.com>
2021-05-27 16:31:01 +0530

    add new approach

--- gfg/005.py
+++ gfg/005.py
@@ -2,13 +2,13 @@
 #code without algo---------------
 def main():
 	arr = list(map(int, input().split()))
-    #a=[]
-    #for i in range(len(arr)):
-    #    if arr[i] < 0:
-    #        a.insert(1, arr[i])
-    #    else:
-    #        a.insert(len(a), arr[i])
-    #print(a)
+    a=[]
+    for i in range(len(arr)):
+       if arr[i] < 0:
+           a.insert(1, arr[i])
+       else:
+           a.insert(len(a), arr[i])
+    print(a)
 #quick sort-------------------	
 	temp=None
 	j=0
--- gfg/006.py
+++ gfg/006.py
@@ -1,2 +1,34 @@
 def doUnion(a,n,b,m):
 	return len(set(a+b))
+#========================
+
+#User function Template for python3
+
+class Solution:
+    #Function to return the count of number of elements in union of two arrays.
+        #code here
+    def doUnion(self,a,n,b,m):
+        arr = []
+        arr = a
+        for i in range(len(b)):
+            if b[i] not in arr:
+                arr.append(b[i])
+        return len(set(arr))
+
+#{
+#  Driver Code Starts
+#Initial Template for Python 3
+
+#contributed by RavinderSinghPB
+if __name__=='__main__':
+    t=int(input())
+    for _ in range(t):
+
+        n,m=[int(x) for x in input().strip().split()]
+
+        a=[int(x) for x in input().strip().split()]
+        b=[int(x) for x in input().strip().split()]
+        ob=Solution()
+
+        print(ob.doUnion(a,n,b,m))
+# } Driver Code Ends
