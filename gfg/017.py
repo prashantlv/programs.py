7798b16e6b8131a9cd87950da39aac9f2ea51ca5
maverick <shukla.prashant689@gmail.com>
2021-06-05 17:11:46 +0530

    ..

--- gfg/017.py
+++ gfg/017.py
@@ -11,16 +11,14 @@ def maxProfit(self, prices: List[int]) -> int:
     return max
 
 #########ONE PASS#############
-#Graph concept
-#find the largest peak following the smallest valley. We can maintain two variables - minprice and maxprofit corresponding to the smallest valley and maximum profit (maximum difference between selling price and minprice) obtained so far respectively.
 
-def maxProfit(self, prices: List[int]) -> int: 
+def maxProfit(self, prices: List[int]) -> int:
     l = len(prices)
-    minprice = max(prices) +1
-    maxprofit = 0 
+    minimum = 2222222222
+    maximum = 0
     for i in range(l):
-        if (prices[i] < minprice):
-            minprice = prices[i]
-        elif (prices[i]-minprice > maxprofit):
-            maxprofit = prices[i] - minprice
-    return maxprofit
+        if (prices[i] < minimum):
+            minimum = prices[i]
+        elif (prices[i]-minimum > maximum):
+            maximum = prices[i] - minimum
+    return maximum        
