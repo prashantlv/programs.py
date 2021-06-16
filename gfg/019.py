defbcaea4bb3d653843d5c6d7d6b0931112da1ad
maverick <shukla.prashant689@gmail.com>
2021-06-16 22:00:11 +0530

    add new logic

--- gfg/019.py
+++ gfg/019.py
@@ -14,3 +14,19 @@ def commonElements (self,A, B, C, n1, n2, n3):
                 if element not in arr:
                     arr.append(element)
         return arr
+
+# Another approach intersection b/w 2 array
+
+def commonElements (self,A, B, C, n1, n2, n3):
+        # your code here
+        arr =[]
+        i, j = 0, 0
+        while (i<n1 and j<n2):
+            if A[i] < B[j]:
+                i += 1
+            elif B[j] < A[i]:
+                j += 1
+            else:
+                arr.append
+        arr = [arr.remove(element) for element in arr if element not in C]
+        return arr
