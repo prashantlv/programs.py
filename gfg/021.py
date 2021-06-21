cebd6b19b89883805c72e39bc2f6f9889a56abaa
maverick <shukla.prashant689@gmail.com>
2021-06-21 22:45:16 +0530

    add fle

---
+++ gfg/021.py
@@ -0,0 +1,37 @@
+class Solution:
+    
+    #Function to check whether there is a subarray present with 0-sum or not.
+    def subArrayExists(self,arr,n):
+        ##Your code here
+        #Return true or false
+        sum, flag = 0, 'No'
+        for i in range(n):
+            for j in range(i,n):
+                sum += arr[j]
+                if sum == 0:
+                    flag = 'Yes'
+        return flag
+
+#{ 
+#  Driver Code Starts
+#Initial Template for Python 3
+
+
+
+def main():
+    T=int(input())
+    while(T>0):
+        
+        n=int(input())
+        arr=[int(x) for x in input().strip().split()]
+        if(Solution().subArrayExists(arr,n)):
+            print("Yes")
+        else:
+            print("No")
+        
+        
+        T-=1
+
+if __name__=="__main__":
+    main()
+# } Driver Code Ends
