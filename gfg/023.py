a91764f11c9d380bafb96f88be8255fdeabe6e7f
maverick <shukla.prashant689@gmail.com>
2021-06-22 13:11:17 +0530

    improve code

--- gfg/023.py
+++ gfg/023.py
@@ -1,13 +1,20 @@
-def maxProduct(self,arr, n):
-    # code here
-    prod, temp = 1, arr[0]
-    for i in range(n):
-        if arr[i] == 0: # if 0 arrives in middel
-            prod = 1
-        else:
-            prod = prod*arr[i]
-        if prod > temp:
-            temp = prod
-    return temp
-
 # failing in test case with first negative and rest positive elements // need to skip those numbers from front    
+
+class Solution:
+    def maxProduct(self,arr, n):
+        prod, temp = 1, arr[0]
+        for i in range(n):
+            if arr[i] == 0:# if 0 arrives in middel
+                prod = 1
+            else:
+                prod = prod*arr[i]
+            if prod > temp:
+                temp = prod
+        return temp
+
+if __name__ == '__main__':
+    n = int(input())
+    arr = list(map(int, input().strip().split()))
+    ob = Solution()
+    ans = ob.maxProduct(arr, n)
+    print(ans)
